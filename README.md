04a13082e0485d22dc94b07b0a597609fd4b219c
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-10-17 07:17:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+A quick and dirty script to install my most used programs on a new Ubuntu system
+This installs
+-gcc-arm-none-eabi
+-Chrome
+-VSCode
+-Nodejs
+-Python3 and pip
+-OpenOCD
+-lm4flash
---
+++ dev-env-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt install git vim  python3 python3-pip gcc-arm-none-eabi gdb-multiarch build-essential \
+flex bison libgmp3-dev libmpfr-dev libncurses5-dev libmpc-dev autoconf texinfo \
+libftdi-dev python-yaml zlib1g-dev minicom putty curl libusb-1.0-0-dev \
+
+#Chrome install
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update
+sudo apt install google-chrome-stable
+
+#VScode install
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
+#nodeJS Install
+wget -qO- https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#lm4flash install
+git clone https://github.com/utzig/lm4tools.git
+cd lm4tools/lm4flash
+make
+sudo cp lm4flash /usr/bin/
+
+#OpenOCD install
+git clone git://git.code.sf.net/p/openocd/code openocd.git
+cd openocd.git
+./bootstrap
+./configure --prefix=/usr --enable-maintainer-mode --enable-stlink --enable-ti-icdi
+make
+sudo make install
