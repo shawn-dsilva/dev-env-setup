911f8ef116f39fa2b251e7feb991bb03e199c6db
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-11-18 11:04:47 +0530

    changes

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-sudo apt install git vim  python3 python3-pip gcc-arm-none-eabi gdb-multiarch build-essential \
+sudo apt install git vim gcc g++ make curl python3 python3-pip gcc-arm-none-eabi gdb-multiarch build-essential ubuntu-make \
 flex bison libgmp3-dev libmpfr-dev libncurses5-dev libmpc-dev autoconf chrome-gnome-shell texinfo \
-libftdi-dev python-yaml zlib1g-dev arc-theme minicom putty curl gnome-shell-extensions gnome-tweak-tool libusb-1.0-0-dev \
+libftdi-dev python-yaml zlib1g-dev minicom putty gnome-shell-extensions gnome-tweak-tool libusb-1.0-0-dev  apt-transport-https ca-certificates  software-properties-common \
 
-#Chrome install
+#Chrome repo
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt update
-sudo apt install google-chrome-stable
 
-#VScode install
+#VScode repo
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install code
 
-#nodeJS Install
+#nodeJS repo
 wget -qO- https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
-#lm4flash install
-git clone https://github.com/utzig/lm4tools.git
-cd lm4tools/lm4flash
-make
-sudo cp lm4flash /usr/bin/
+#Docker repo
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+#MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+
+#Paper icon theme repo
+sudo add-apt-repository -u ppa:snwh/ppa -y
+
+
+sudo apt update 
+sudo apt install  -y google-chrome-stable nodejs code mongodb-org docker-ce  arc-theme paper-icon-theme
+
+
+#IntelliJ
+
+umake ide idea
 
 #OpenOCD install
 git clone git://git.code.sf.net/p/openocd/code openocd.git
@@ -35,7 +44,4 @@ cd openocd.git
 make
 sudo make install
 
-#Paper icon theme install
-sudo add-apt-repository -u ppa:snwh/ppa
-sudo apt-get install paper-icon-theme
 
