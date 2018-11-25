59d5c6ac6397be397332d1b589f1f3f0b44b7641
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2018-11-25 12:53:04 +0530

    npm and vscode ext install

--- dev-env-setup.sh
+++ dev-env-setup.sh
@@ -28,6 +28,8 @@ echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/
 sudo add-apt-repository -u ppa:snwh/ppa -y
 
 
+# Apt update and installs
+
 sudo apt update 
 sudo apt install  -y google-chrome-stable nodejs code mongodb-org docker-ce docker-compose arc-theme paper-icon-theme
 
@@ -43,5 +45,31 @@ cd openocd.git
 ./configure --prefix=/usr --enable-maintainer-mode --enable-stlink --enable-ti-icdi
 make
 sudo make install
+
+#npm global installs
+
+sudo npm install -g @angular/cli
+sudo npm install -g typescript
+sudo npm install -g eslint
+sudo npm install -g tslint
+sudo npm install -g nodemon
+
+
+#VScode extensions install
+
+code --install-extension azemoh.one-monokai
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dracula-theme.theme-dracula
+code --install-extension eg2.tslint
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension HookyQR.beautify
+code --install-extension ms-azuretools.vscode-cosmosdb
+code --install-extension ms-python.python
+code --install-extension ms-vscode.azure-account
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension PKief.material-icon-theme
+code --install-extension shakram02.bash-beautify
+code --install-extension zhuangtongfa.Material-theme
+
 
 
